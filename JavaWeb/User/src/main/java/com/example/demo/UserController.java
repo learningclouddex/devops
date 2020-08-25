f7758515c4266a500ed6fe7fa86c5129ea20fec0
Sivanesan Tamilmani <ST00687225@techmahindra.com>
2020-08-25 19:42:32 +0530

    added java web app

---
+++ JavaWeb/User/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserRepository userRepo;
+
+    @RequestMapping("/users")
+    public List<User> home(Model model) {
+         return userRepo.findAll();
+    }
+}
